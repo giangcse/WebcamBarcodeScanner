693b73412f364251da1e381bf200829eafe5729d
giangcse <giangphan028@gmail.com>
2025-08-13 10:29:58 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WebcamBarcodeScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerScan = new System.Windows.Forms.Timer(this.components);
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cboDevices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.dtpToDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpFromDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picVideo = new System.Windows.Forms.PictureBox();
+            this.panelTop.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerScan
+            // 
+            this.timerScan.Interval = 500;
+            this.timerScan.Tick += new System.EventHandler(this.timerScan_Tick);
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.btnStart);
+            this.panelTop.Controls.Add(this.cboDevices);
+            this.panelTop.Controls.Add(this.label1);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(10, 10);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(764, 44);
+            this.panelTop.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(654, 7);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 28);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cboDevices
+            // 
+            this.cboDevices.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDevices.FormattingEnabled = true;
+            this.cboDevices.Location = new System.Drawing.Point(107, 9);
+            this.cboDevices.Name = "cboDevices";
+            this.cboDevices.Size = new System.Drawing.Size(541, 24);
+            this.cboDevices.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn Camera:";
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.groupBox1);
+            this.panelBottom.Controls.Add(this.dgvHistory);
+            this.panelBottom.Controls.Add(this.label5);
+            this.panelBottom.Controls.Add(this.txtResult);
+            this.panelBottom.Controls.Add(this.label2);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(10, 361);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(764, 290);
+            this.panelBottom.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnExport);
+            this.groupBox1.Controls.Add(this.dtpToDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dtpFromDate);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 228);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 59);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Xuất Báo Cáo";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(639, 21);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(113, 28);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Xuất Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // dtpToDate
+            // 
+            this.dtpToDate.CustomFormat = "dd/MM/yyyy";
+            this.dtpToDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpToDate.Location = new System.Drawing.Point(246, 24);
+            this.dtpToDate.Name = "dtpToDate";
+            this.dtpToDate.Size = new System.Drawing.Size(129, 22);
+            this.dtpToDate.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Đến:";
+            // 
+            // dtpFromDate
+            // 
+            this.dtpFromDate.CustomFormat = "dd/MM/yyyy";
+            this.dtpFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFromDate.Location = new System.Drawing.Point(40, 24);
+            this.dtpFromDate.Name = "dtpFromDate";
+            this.dtpFromDate.Size = new System.Drawing.Size(129, 22);
+            this.dtpFromDate.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Từ:";
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AllowUserToAddRows = false;
+            this.dgvHistory.AllowUserToDeleteRows = false;
+            this.dgvHistory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(3, 72);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.ReadOnly = true;
+            this.dgvHistory.RowHeadersVisible = false;
+            this.dgvHistory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvHistory.Size = new System.Drawing.Size(758, 150);
+            this.dgvHistory.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Lịch sử quét:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(3, 22);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(758, 22);
+            this.txtResult.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kết quả quét gần nhất:";
+            // 
+            // picVideo
+            // 
+            this.picVideo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picVideo.Location = new System.Drawing.Point(10, 54);
+            this.picVideo.Name = "picVideo";
+            this.picVideo.Size = new System.Drawing.Size(764, 307);
+            this.picVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picVideo.TabIndex = 1;
+            this.picVideo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 800);
+            this.Controls.Add(this.picVideo);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelTop);
+            this.MinimumSize = new System.Drawing.Size(800, 700);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam Barcode & QR Scanner";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panelBottom.ResumeLayout(false);
+            this.panelBottom.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerScan;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cboDevices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picVideo;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvHistory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.DateTimePicker dtpToDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpFromDate;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ClosedXML.Excel;
+using ZXing;
+
+namespace WebcamBarcodeScanner
+{
+    public partial class Form1 : Form
+    {
+        // Khai báo các biến toàn cục
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private ResultPoint[] lastResultPoints;
+
+        // Khai báo đường dẫn file CSDL
+        private static readonly string dbFile = "ScanHistory.sqlite";
+        private static readonly string connectionString = $"Data Source={dbFile};Version=3;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sự kiện khi Form được tải lên
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeDatabase();
+            LoadDataToGrid();
+            InitializeCamera();
+        }
+
+        #region Database Methods
+
+        // Khởi tạo CSDL và tạo/cập nhật bảng nếu cần
+        private void InitializeDatabase()
+        {
+            if (!File.Exists(dbFile))
+            {
+                SQLiteConnection.CreateFile(dbFile);
+            }
+
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                // Tạo bảng nếu chưa có
+                string createTableSql = "CREATE TABLE IF NOT EXISTS ScanHistory (ID INTEGER PRIMARY KEY AUTOINCREMENT, Result TEXT, ScanDate TEXT, ScanTime TEXT)";
+                using (var cmd = new SQLiteCommand(createTableSql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Kiểm tra và thêm cột ScanTime nếu bảng cũ chưa có
+                var cmdCheck = new SQLiteCommand("PRAGMA table_info(ScanHistory)", conn);
+                var reader = cmdCheck.ExecuteReader();
+                bool columnExists = false;
+                while (reader.Read())
+                {
+                    if (reader["name"].ToString().Equals("ScanTime", StringComparison.OrdinalIgnoreCase))
+                    {
+                        columnExists = true;
+                        break;
+                    }
+                }
+                reader.Close();
+
+                if (!columnExists)
+                {
+                    var cmdAlter = new SQLiteCommand("ALTER TABLE ScanHistory ADD COLUMN ScanTime TEXT", conn);
+                    cmdAlter.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Thêm kết quả quét vào CSDL
+        private void InsertScanResult(string result)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO ScanHistory (Result, ScanDate, ScanTime) VALUES (@result, @scanDate, @scanTime)";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@result", result);
+                    cmd.Parameters.AddWithValue("@scanDate", DateTime.Now.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@scanTime", DateTime.Now.ToString("HH:mm:ss"));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Tải dữ liệu lên DataGridView
+        private void LoadDataToGrid()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT ID, Result, ScanDate, ScanTime FROM ScanHistory ORDER BY ID DESC";
+                using (var adapter = new SQLiteDataAdapter(sql, conn))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dgvHistory.DataSource = dt;
+
+                    // Tùy chỉnh tiêu đề và độ rộng cột
+                    if (dgvHistory.Columns.Count > 0)
+                    {
+                        dgvHistory.Columns["ID"].HeaderText = "STT";
+                        dgvHistory.Columns["Result"].HeaderText = "Kết quả";
+                        dgvHistory.Columns["ScanDate"].HeaderText = "Ngày quét";
+                        dgvHistory.Columns["ScanTime"].HeaderText = "Giờ quét";
+
+                        dgvHistory.Columns["ID"].FillWeight = 10;
+                        dgvHistory.Columns["Result"].FillWeight = 50;
+                        dgvHistory.Columns["ScanDate"].FillWeight = 20;
+                        dgvHistory.Columns["ScanTime"].FillWeight = 20;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Camera Control
+
+        private void InitializeCamera()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("Không tìm thấy webcam nào!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnStart.Enabled = false;
+                return;
+            }
+            foreach (FilterInfo device in videoDevices)
+            {
+                cboDevices.Items.Add(device.Name);
+            }
+            cboDevices.SelectedIndex = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning) StopCamera();
+            else StartCamera();
+        }
+
+        private void StartCamera()
+        {
+            try
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[cboDevices.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                videoSource.Start();
+                timerScan.Start();
+                btnStart.Text = "Dừng";
+                cboDevices.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi khởi động camera: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StopCamera()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                timerScan.Stop();
+                videoSource.NewFrame -= new NewFrameEventHandler(video_NewFrame);
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+                lastResultPoints = null;
+                picVideo.Image = null;
+                btnStart.Text = "Bắt đầu";
+                cboDevices.Enabled = true;
+            }
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+            if (lastResultPoints != null && lastResultPoints.Length > 0)
+            {
+                Point[] points = lastResultPoints.Select(p => new Point((int)p.X, (int)p.Y)).ToArray();
+                using (Graphics g = Graphics.FromImage(frame))
+                {
+                    using (Pen pen = new Pen(Color.LimeGreen, 3))
+                    {
+                        g.DrawPolygon(pen, points);
+                    }
+                }
+            }
+            picVideo.Image = frame;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCamera();
+        }
+
+        #endregion
+
+        #region Scanning and Export
+
+        private void timerScan_Tick(object sender, EventArgs e)
+        {
+            if (picVideo.Image == null) return;
+
+            BarcodeReader reader = new BarcodeReader { Options = { TryHarder = true } };
+            Result result = reader.Decode((Bitmap)picVideo.Image);
+
+            if (result != null)
+            {
+                lastResultPoints = result.ResultPoints;
+                txtResult.Invoke(new MethodInvoker(delegate ()
+                {
+                    string decodedText = result.ToString();
+                    if (txtResult.Text != decodedText)
+                    {
+                        txtResult.Text = decodedText;
+                        Clipboard.SetText(decodedText);
+                        System.Media.SystemSounds.Beep.Play();
+                        InsertScanResult(decodedText);
+                        LoadDataToGrid();
+                    }
+                }));
+            }
+            else
+            {
+                lastResultPoints = null;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fromDate = dtpFromDate.Value.ToString("yyyy-MM-dd");
+                string toDate = dtpToDate.Value.ToString("yyyy-MM-dd");
+
+                DataTable dt = new DataTable();
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT Result, ScanDate, ScanTime FROM ScanHistory WHERE ScanDate BETWEEN @fromDate AND @toDate ORDER BY ID ASC";
+                    using (var adapter = new SQLiteDataAdapter(sql, conn))
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("@fromDate", fromDate);
+                        adapter.SelectCommand.Parameters.AddWithValue("@toDate", toDate);
+                        adapter.Fill(dt);
+                    }
+                }
+
+                if (dt.Rows.Count == 0)
+                {
+                    MessageBox.Show("Không có dữ liệu để xuất trong khoảng thời gian đã chọn.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                dt.Columns["Result"].ColumnName = "Kết quả";
+                dt.Columns["ScanDate"].ColumnName = "Ngày quét";
+                dt.Columns["ScanTime"].ColumnName = "Giờ quét";
+
+                using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx", ValidateNames = true })
+                {
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        using (var workbook = new XLWorkbook())
+                        {
+                            workbook.Worksheets.Add(dt, "LichSuQuet");
+                            var worksheet = workbook.Worksheet(1);
+                            worksheet.Columns().AdjustToContents();
+                            workbook.SaveAs(sfd.FileName);
+                        }
+                        MessageBox.Show("Xuất file Excel thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi khi xuất file: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+    }
+}
